e18ae41479053000b79bc6b2d2b0b32e58745da8
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2025-01-16 18:25:48 -0800

    changed to port 80

--- backend/app.py
+++ backend/app.py
@@ -59,7 +59,7 @@ async def hi():
 
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=80, reload=True)
 
 if __name__ == "__main__":
     main()
